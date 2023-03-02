597c54a44cc1fa74b97fcce3a30bd4f1b1aec500
Frédéric Lenois <frl@filmstory.fr>
2023-03-02 23:27:14 +0100

    WIP partage scores

--- src/ScoresButton.svelte
+++ src/ScoresButton.svelte
@@ -1,13 +1,18 @@
 <script>
     import Fa from "svelte-fa/src/fa.svelte";
+    import { faCopy } from '@fortawesome/free-solid-svg-icons';
     import { faTrophy } from "@fortawesome/free-solid-svg-icons";
     import { Button, Modal } from "sveltestrap";
     import { scoreBoard } from "./services/stores";
-    import { numberEmojis } from "./services/DepartementService";
+    import { numberEmojis, clipboardShare } from "./services/DepartementService";
     import { config } from "./referentiel/departements.json"
 
     let isOpen = false;
     const toggle = () => (isOpen = !isOpen);
+
+    const buildShareData = () => {
+        return "Hello world"
+    }
 </script>
 
 <Button color="success" id="clipboard-button" on:click={toggle}>
@@ -23,4 +28,18 @@
         {/if}
         <br/>
     {/each}
+    <div class="text-center mt-3">
+    <Button
+        color="success"
+        id="clipboard-button"
+        on:click={(e) => {clipboardShare(buildShareData, e.target.id )}}>
+            Copier <Fa icon={faCopy} />
+        </Button>
+    </div>
 </Modal>
+
+<style>
+    .text-center {
+        width: 100%;
+    }
+</style>
